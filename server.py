10c423705b0ec1172aa0a1b9190fa71613c4b957
Alex Reyes <akosialexpo@gmail.com>
2020-05-04 01:11:14 +0800

    Add link validation

--- server.py
+++ server.py
@@ -1,41 +1,52 @@
 #!/Users/alex/Code/clock-in-bot/venv/bin
 
-import clock_in_bot
 from flask import Flask
+import ast
+import clock_in_bot
 
 
 app = Flask(__name__)
 bot = clock_in_bot.ClockInBot()
+resp = None
+log = []
 
 @app.route("/clock-in")
 def clock_in():
     try:
         print("Clocking in...")
+        global resp
+        if get_log_request(resp) not in log: raise Exception("Link expired.")
         bot.login()
         bot.driver.implicitly_wait(10)
         bot.clock_in()
         bot.driver.implicitly_wait(10)
         bot.get_screenshot()
-        resp = bot.send_notification()
-        if not resp: raise Exception("resp is undefined for some reason..")
+        respClock = bot.send_notification()
+        if not respClock: raise Exception("resp is undefined for some reason..")
     except NewConnectionError:
         return "Could not connect. Check server logs."
     finally:
         print("Done.")
-        return resp.reason
+        if resp.ok: log.remove(get_log_request(resp))
+        return respClock.reason
 
 @app.route("/status")
 def get_status():
-    return bot.is_clocked_in
+    print(log)
+    return "Currently clocked {}".format("in" if bot.is_clocked_in else "out")
 
 
 @app.route("/")
 def send_request():
     try:
         print("Sending request...")
+        global resp
         resp = bot.send_request()
+        log.append(get_log_request(resp))
     finally:
         print("Done.")
+        print(log)
         return resp.reason
 
-
+def get_log_request(resp):
+    return ast.literal_eval(resp.content.decode('utf8'))['request']
